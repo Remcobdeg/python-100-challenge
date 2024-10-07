2d89232941d5997643b7b2e9328e8b1ab36038e7
Remco de Grave <remcodegrave@gmail.com>
2024-10-07 15:47:31 +0100

    updates nato alphabet with a keyerror exception

--- day-26-NATO-alphabet/main.py
+++ day-26-NATO-alphabet/main.py
@@ -5,12 +5,18 @@ alphabet_df = pd.read_csv("nato_phonetic_alphabet.csv")
 # alphabet_dict = {row.values[0]:row.values[1] for (index,row) in alphabet_df.iterrows()}
 alphabet_dict = {row.letter:row.code for (index,row) in alphabet_df.iterrows()}
 
+def ask_word():
+    return input("Give a word to convert to phonetic code\n")
 
-print(alphabet_dict)
+completed = False
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Give a word to convert to phonetic code\n")
+while not completed:
+    word = ask_word()
+    try:
+        output = [alphabet_dict[letter] for letter in word.upper()]
+    except KeyError:
+        print("Only words please")
+    else:
+        print(output)
+        completed = True
 
-output = [alphabet_dict[letter] for letter in word.upper()]
-
-print(output)
