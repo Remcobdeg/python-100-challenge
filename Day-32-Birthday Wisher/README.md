107a6aaf38d6b978b56e03bb38722f41a8a62352
Remco de Grave <remcodegrave@gmail.com>
2024-10-11 20:11:33 +0200

    day 32 simplest way of sending an email from Python

---
+++ Day-32-Birthday Wisher/README.md
@@ -0,0 +1,5 @@
+To be able to send an email through gmail, we have to create an app password. 
+* Go to settings > security
+* Setup 2FA
+* Search for 'app password'
+* Create one
---
+++ Day-32-Birthday Wisher/main.py
@@ -0,0 +1,15 @@
+import smtplib
+from dotenv import load_dotenv
+import os
+
+# Load the environment variables from the .env file
+load_dotenv()
+
+my_email = "remco.b.de.grave@gmail.com"
+password=os.getenv('APP_PASSWORD')
+
+connection = smtplib.SMTP("smtp.gmail.com") #smtp.mail.yahoo.com
+connection.starttls() #making encrypted connection to email server
+connection.login(user=my_email, password=password)
+connection.sendmail(from_addr=my_email, to_addrs="remco.degrave@yahoo.com", msg="hello")
+connection.close()
