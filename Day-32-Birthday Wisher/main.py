614ddfed708a55bea1d5db2efd7871585b002176
Remco de Grave <remcodegrave@gmail.com>
2024-10-11 20:19:53 +0200

    day 32 use 'with' for auto-close

--- Day-32-Birthday Wisher/main.py
+++ Day-32-Birthday Wisher/main.py
@@ -8,9 +8,8 @@ load_dotenv()
 my_email = "remco.b.de.grave@gmail.com"
 password=os.getenv('APP_PASSWORD')
 
-connection = smtplib.SMTP("smtp.gmail.com") #smtp.mail.yahoo.com
-connection.starttls() #making encrypted connection to email server
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="remco.degrave@yahoo.com", msg="Subject:hello\n\n"
-                                                                                "This is the body of my email")
-connection.close()
+with smtplib.SMTP("smtp.gmail.com") as connection: #or, for yahoo: smtp.mail.yahoo.com
+    connection.starttls() #making encrypted connection to email server
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email, to_addrs="remco.degrave@yahoo.com", msg="Subject:hello\n\n"
+                                                                                    "This is the body of my email")
