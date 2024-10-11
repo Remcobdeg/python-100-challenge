a372c8237198826d293a98d352d17bc651204a73
Remco de Grave <remcodegrave@gmail.com>
2024-10-11 20:18:49 +0200

    day 32 add title to message

--- Day-32-Birthday Wisher/main.py
+++ Day-32-Birthday Wisher/main.py
@@ -11,5 +11,6 @@ password=os.getenv('APP_PASSWORD')
 connection = smtplib.SMTP("smtp.gmail.com") #smtp.mail.yahoo.com
 connection.starttls() #making encrypted connection to email server
 connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="remco.degrave@yahoo.com", msg="hello")
+connection.sendmail(from_addr=my_email, to_addrs="remco.degrave@yahoo.com", msg="Subject:hello\n\n"
+                                                                                "This is the body of my email")
 connection.close()
