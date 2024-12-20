e72a8ffe302ef090da8fd39136aa501f38793f56
Remco Benthem de Grave <rbenthemdegrave@rosen-group.com>
2024-12-20 20:36:50 +0000

    update day 32 with absolute path

--- Day-32-Birthday Wisher/main.py
+++ Day-32-Birthday Wisher/main.py
@@ -4,11 +4,14 @@ from dotenv import load_dotenv
 import os
 import random
 
+# get dircetory of the file
+dir = os.path.dirname(__file__)
+
 # Load the environment variables from the .env file
 load_dotenv()
 
 #get quotes
-with open("quotes.txt") as file:
+with open(os.path.join(dir,"quotes.txt")) as file:
     quotes = [line.strip() for line in file.readlines()]
 
 my_email=os.getenv('EMAIL')
@@ -17,7 +20,7 @@ password=os.getenv('APP_PASSWORD')
 week_day = dt.datetime.now().weekday() #Monday = 0, Sunday = 6
 quote = random.choice(quotes)
 
-if week_day == 0:
+if week_day == 4:
     print(f"Sending quote")
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls() #making encrypted connection to email server
