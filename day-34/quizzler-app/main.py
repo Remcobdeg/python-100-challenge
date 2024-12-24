c606c274352e5ca94d2ec091de5037c215cf6ac0
Remco Benthem de Grave <remcodegrave@gmail.com>
2024-12-24 22:07:17 +0200

    TK inter UI for quizler (34)

--- day-34/quizzler-app/main.py
+++ day-34/quizzler-app/main.py
@@ -1,7 +1,13 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 import requests
+import html
+import os
+
+# set working directory to the location of this file
+os.chdir(os.path.dirname(__file__))
 
 # collect question data
 try:
@@ -17,6 +23,7 @@ else:
 question_bank = []
 for question in question_data:
     question_text = question["question"]
+    question_text = html.unescape(question_text)
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
@@ -24,8 +31,10 @@ for question in question_data:
 
 quiz = QuizBrain(question_bank)
 
-while quiz.still_has_questions():
-    quiz.next_question()
+quizInterface = QuizInterface()
+
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- day-34/quizzler-app/ui.py
+++ day-34/quizzler-app/ui.py
@@ -1,2 +1,40 @@
+import tkinter as tk
+import os
+
 THEME_COLOR = "#375362"
+THEME_FONT = 'Arial'
+
+app_path = os.path.dirname(__file__)
+
+class QuizInterface:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.card = tk.Canvas(width=300, height=250, background="white")
+        self.card_text = self.card.create_text(150,125, text='quiz', font=(THEME_FONT, 20, "italic"), fill="black")
+        self.card.grid(column=1,row=2,columnspan = 2, pady=50)
+
+        self.score_count = tk.Label(text="Score: 0", font=(THEME_FONT, 10, "bold"), bg=THEME_COLOR, fg="white")
+        self.score_count.grid(column=2, row=1)
+
+        false_image = tk.PhotoImage(file = os.path.join(app_path,'images/false.png'))
+        self.false_button = tk.Button(image=false_image, highlightthickness=0, bd=0, command=self.is_false)
+        self.false_button.grid(column=2, row=3)
+
+        true_image = tk.PhotoImage(file = os.path.join(app_path,'images/true.png'))
+        self.true_button = tk.Button(image=true_image, highlightthickness=0, bd=0, command=self.is_true)
+        self.true_button.grid(column=1, row=3)
+
+        self.window.mainloop()
+
+    def is_true(self):
+        pass
+
+    def is_false(self):
+        pass
+
+
+
 
