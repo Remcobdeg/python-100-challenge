ac6366b8a6f1d088a814f568ac7fc3f6945019a9
Remco Benthem de Grave <remcodegrave@gmail.com>
2024-12-28 22:14:18 +0200

    day 36 concatenate message

--- day-36-stock-news-extrahard/main.py
+++ day-36-stock-news-extrahard/main.py
@@ -30,44 +30,32 @@ day_before_yesterday = dates[1]
 open_yesterday = float(stock_day_data[yesterday]["1. open"])
 close_day_before_yesterday = float(stock_day_data[day_before_yesterday]["4. close"])
 
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then ...
-percentage_change = (open_yesterday - close_day_before_yesterday) / close_day_before_yesterday * 100
+# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then collect news articles
+percentage_change = round((open_yesterday - close_day_before_yesterday) / close_day_before_yesterday * 100, ndigits=1)
 
 if abs(percentage_change) > .5:
-    print("Get News")
-    
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
-params = {
-    "q": COMPANY_NAME,
-    "from": day_before_yesterday,
-    "sortBy": "popularity",
-    "language": "en",
-    "pageSize": 3,
-    "apiKey": NEWS_API_KEY
-}
+    params = {
+        "q": COMPANY_NAME,
+        "from": day_before_yesterday,
+        "sortBy": "popularity",
+        "language": "en",
+        "pageSize": 3,
+        "apiKey": NEWS_API_KEY
+    }
 
-news_articles = requests.get("https://newsapi.org/v2/everything", params=params)
-news_articles.raise_for_status()
+    news_articles = requests.get("https://newsapi.org/v2/everything", params=params)
+    news_articles.raise_for_status()
+    news_articles = news_articles.json()["articles"]
 
-news_articles = news_articles.json()["articles"]
+    messages = [
+        f"{STOCK}: {'🔺' if percentage_change > 0 else '🔻'}{abs(percentage_change)}%\nHeadline: {article['title']}\nBrief: {article['description']}"
+        for article in news_articles
+    ]
 
-print(news_articles)
+    # concatenate all messages
+    messages = "\n\n".join(messages)
 
+    print(messages)
 
-## STEP 3: Use https://www.twilio.com
-# Send a separate message with the percentage change and each article's title and description to your phone number. 
-
-
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
 
